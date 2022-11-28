50d5e150f985907a5789c0fb01b6d0db3c3d1bb6
moyinoluwaa <dayoh14@gmail.com>
2022-11-28 15:20:46 +0100

    New UI redesign

---
+++ src/pages/home.jsx
@@ -0,0 +1,100 @@
+import brands from "../Assets/images/brands.jpg";
+import logo from "../Assets/svgs/logo.svg";
+import art from "../Assets/svgs/artboard.svg";
+import drinkResponsibly from "../Assets/svgs/drink__responsibly.svg";
+import { Link } from "react-router-dom";
+import { routes } from "../routes"
+// import abinbev from "./Assets/svgs/abi__nigeria.svg"
+import twitter from "../Assets/svgs/twitter.svg";
+import instagram from "../Assets/svgs/Instagram.svg"
+import facebook from "../Assets/svgs/facebook.svg";
+import whatsapp from "../Assets/images/whatsapp.png";
+import cart from "../Assets/svgs/cart.svg"
+import telephone from "../Assets/svgs/phone.svg"
+import Modal from "../components/Modal";
+
+const formerHome = () => {
+    const navigate = () => {
+        window.open('https://paystack.shop/drinks-on-bevz');
+    }
+    
+    return (
+        <div className="brewery">
+            <section className="brewery__section">
+                <div>
+                    <div className="logo__container">
+                    <img className="logo__container__drive" src={logo} alt="" />
+                    </div>
+                    <div className="text-center">
+                    <span className="showcase__text first">
+                        Your <span className="emphasize">FAVOURITE</span> drinks,
+                    </span>
+                    </div>
+                    <div className="text-center">
+                    <span className="showcase__text second">
+                        <span className="emphasize">COLD,</span> in minutes & ready to{" "}
+                    </span>
+                    </div>
+                    <div className="text-center">
+                    <span className="showcase__text third">
+                        <span className="emphasize">DRINK</span> !
+                    </span>
+                    </div>
+                </div>
+            </section>
+            <section className="brewery__section">
+            <div className="brewery__section__item">
+                <div className="grid-section">
+                <div className="grid-section__buttons">
+                    <button style={{ cursor: "pointer"}} onClick={navigate} className="grid-section__buttons__button">
+                    <img src={cart} alt="" />
+                    <span>Shop Now</span>
+                    </button>
+                    <button style={{ cursor: "pointer"}} className="grid-section__buttons__button white">
+                    <img src={telephone} alt="" />
+                    <span>Contact Us</span>
+                    </button>
+                </div>
+                {/* <div className="abinbev__desktop">
+                    <img src={abinbev} alt="" />
+                </div> */}
+                </div>
+                <div className="grid-section">
+                <div className="grid-section__img">
+                    <img src={art} alt="" />
+                </div>
+                {/* <div className="abinbev__mobile">
+                    <img src={abinbev} alt="" />
+                </div> */}
+                </div>
+            </div>
+            <div className="brewery__section__item">
+                <img src={brands} alt="" />
+                <div className="transparent__white" />
+                <div className="faded__bottom" />
+                <div className="footer">
+                <div className="footer__socials">
+                    <a href="https://twitter.com/drinksonbevz?s=11&t=xEQjoU9a0UFcKTy-COvo9g" target="_blank" rel="noreferrer" >
+                    <img src={twitter} alt="twitter link" />
+                    </a>
+                    <a href="https://instagram.com/drinksonbevz?igshid=YmMyMTA2M2Y=" target="_blank" rel="noreferrer">
+                    <img src={instagram} alt="instagram link" />
+                    </a>
+                    <img src={facebook} alt="" />
+                    <img src={whatsapp} alt="" />
+                </div>
+                <p style={{ justifyContent: "center", fontSize: "14px", fontWeight: "bold"}} className="footer__caution">Copyright &copy;BEVZ 2022. All rights reserved. Alcohol is not for sale to persons under the Age of 18.</p>
+                <Link style={{ justifyContent: "center", fontSize: "14px", fontWeight: "bold", color: "black", zIndex: "5"}} className="footer__caution"  to={routes.TermsAndConditions}>
+                    Terms, Conditions & Privacy Policy 
+                </Link>
+                <div className="footer__caution">
+                    <img src={drinkResponsibly} alt="" />
+                </div>
+                </div>
+            </div>
+            </section>
+        </div>
+    )
+}
+
+export default formerHome;
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,6 +2,8 @@ import brands from "../Assets/images/brands.jpg";
 import logo from "../Assets/svgs/logo.svg";
 import art from "../Assets/svgs/artboard.svg";
 import drinkResponsibly from "../Assets/svgs/drink__responsibly.svg";
+import { Link } from "react-router-dom";
+import { routes } from "../routes"
 // import abinbev from "./Assets/svgs/abi__nigeria.svg"
 import twitter from "../Assets/svgs/twitter.svg";
 import instagram from "../Assets/svgs/Instagram.svg"
@@ -10,7 +12,18 @@ import whatsapp from "../Assets/images/whatsapp.png";
 import cart from "../Assets/svgs/cart.svg"
 import telephone from "../Assets/svgs/phone.svg"
 import Modal from "../components/Modal";
-
+import Button from "../components/Button";
+import herobannerImage from "../Assets/images/beer-group.png"
+import trophyStout from "../Assets/images/brands/trophystout.png";
+import flyingFish from "../Assets/images/brands/flyingfish.png";
+import budweiser from "../Assets/images/brands/budweiser.png";
+import hero from "../Assets/images/brands/herolager.png";
+import castleLite from "../Assets/images/brands/castlelite.png";
+import trophy from "../Assets/images/brands/trophy.png";
+import flyingFishf from "../Assets/images/flyingfishes.png";
+import budweiserf from "../Assets/images/budweiser-f.png";
+import footerLogo from "../Assets/images/logo.png"
+import notUnderAgeIcon from "../Assets/images/underageIcon.png"
 
 function Home() {
   const navigate = () => {
@@ -19,75 +32,86 @@ function Home() {
 
   return (
       <div className="brewery">
-        <section className="brewery__section">
-          <div>
-            <div className="logo__container">
-              <img className="logo__container__drive" src={logo} alt="" />
+        <section className="herobanner">
+          <div className="logo__container">
+            <img className="logo__container__drive" src={logo} alt="logo" />
+          </div>
+          <div className="herobanner__text">
+            <h1>Cold drinks at the best price without leaving home</h1>
+            <p>Get your favourite drinks, COLD, in minutes and ready to drink from the comfort of your home.</p>
+            <div>
+              <Button btnClassName="btn__yellow">Shop now</Button>
+              <Button btnClassName="btn__white">Contact us</Button>
             </div>
-            <div className="text-center">
-              <span className="showcase__text first">
-                Your <span className="emphasize">FAVOURITE</span> drinks,
-              </span>
-            </div>
-            <div className="text-center">
-              <span className="showcase__text second">
-                <span className="emphasize">COLD,</span> in minutes & ready to{" "}
-              </span>
-            </div>
-            <div className="text-center">
-              <span className="showcase__text third">
-                <span className="emphasize">DRINK</span> !
-              </span>
+            <div className="socials">
+              <a href="https://twitter.com/drinksonbevz?s=11&t=xEQjoU9a0UFcKTy-COvo9g" target="_blank" rel="noreferrer" >
+                <img src={twitter} alt="twitter link" />
+              </a>
+              <a href="https://instagram.com/drinksonbevz?igshid=YmMyMTA2M2Y=" target="_blank" rel="noreferrer">
+                <img src={instagram} alt="instagram link" />
+              </a>
+              <img src={facebook} alt="" />
+              <img src={whatsapp} alt="" />
             </div>
           </div>
+          <img className="herobanner__image" src={herobannerImage} alt='premium beer' />
         </section>
-        <section className="brewery__section">
-          <div className="brewery__section__item">
-            <div className="grid-section">
-              <div className="grid-section__buttons">
-                <button style={{ cursor: "pointer"}} onClick={navigate} className="grid-section__buttons__button">
-                  <img src={cart} alt="" />
-                  <span>Shop Now</span>
-                </button>
-                <button style={{ cursor: "pointer"}} className="grid-section__buttons__button white">
-                  <img src={telephone} alt="" />
-                  <span>Contact Us</span>
-                </button>
+        <div className="brands">
+          <img src={trophyStout} alt="trophy stout" />
+          <img src={flyingFish} alt="flying fish" />
+          <img src={budweiser} alt="budweiser" />
+          <img src={hero} alt="hero lager" />
+          <img src={castleLite} alt="castle lite" />
+          <img src={trophy} alt="trophy" />
+          <img src={trophyStout} alt="trophy stout" />
+          <img src={flyingFish} alt="flying fish" />
+          <img src={budweiser} alt="budweiser" />
+          <img src={hero} alt="hero lager" />
+          <img src={castleLite} alt="castle lite" />
+          <img src={trophy} alt="trophy" />
+        </div>
+        <section className="featured-brands">
+          <div className="featured-brands__section">
+            <h2>FEATURED BRANDS</h2>
+            <div className="featured-brands__row">
+              <div className="featured-brands__card">
+                <img src={flyingFishf} alt="flying fish" />
+                <div className="featured-brands__card__text first">
+                  <h3>Enjoy the new Flying Fish beer</h3>
+                  <p>
+                    In every bottle of Flying Fish, you experience the perfect blend of premium beer and fruity flavor, which is 
+                    refreshing and filled with vibrance and pure excitement until the very last drop. Nothing Tastes Like Flying Fish.
+                  </p>
+                  <Button btnClassName="btn__white">Learn more</Button>
+                </div>
               </div>
-              {/* <div className="abinbev__desktop">
-                <img src={abinbev} alt="" />
-              </div> */}
-            </div>
-            <div className="grid-section">
-              <div className="grid-section__img">
-                <img src={art} alt="" />
-              </div>
-              {/* <div className="abinbev__mobile">
-                <img src={abinbev} alt="" />
-              </div> */}
-            </div>
-          </div>
-          <div className="brewery__section__item">
-            <img src={brands} alt="" />
-            <div className="transparent__white" />
-            <div className="faded__bottom" />
-            <div className="footer">
-              <div className="footer__socials">
-                <a href="https://twitter.com/drinksonbevz?s=11&t=xEQjoU9a0UFcKTy-COvo9g" target="_blank" rel="noreferrer" >
-                  <img src={twitter} alt="twitter link" />
-                </a>
-                <a href="https://instagram.com/drinksonbevz?igshid=YmMyMTA2M2Y=" target="_blank" rel="noreferrer">
-                  <img src={instagram} alt="instagram link" />
-                </a>
-                <img src={facebook} alt="" />
-                <img src={whatsapp} alt="" />
-              </div>
-              <div className="footer__caution">
-                <img src={drinkResponsibly} alt="" />
+              <div className="featured-brands__card">
+                <div className="featured-brands__card__text second">
+                  <h3>Budweiser - Brewed smooth just for you</h3>
+                  <p>
+                    With a smooth taste that doesn’t hold you back, Budweiser is brewed smooth for you, our Naija Kings. Offering the
+                    smoothest feeling for the smoothest time.
+                  </p>
+                  <Button btnClassName="btn__white">Learn more</Button>
+                </div>
+                <img src={budweiserf} alt="budweiser" />
               </div>
             </div>
           </div>
         </section>
+        <footer>
+          <div className="footer__row">
+            <div className="footer__copyright">
+              <img src={footerLogo} alt="logo" />
+              <span>Copyright ©BEVZ 2022. All rights reserved. Alcohol is not for sale to persons under the Age of 18.</span>
+            </div>
+            <div className="footer__drink__responsibly">
+              <img src={notUnderAgeIcon} alt="logo" />
+              <span>Drink responsibly</span>
+            </div>
+            <Link to={routes.TermsAndConditions} className="footer__terms">Terms, Conditions & Privacy Policy</Link>
+          </div>
+        </footer>
         <Modal />
       </div>
   )
